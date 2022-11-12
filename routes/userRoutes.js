cbb56e67779f1818646c6c9e85b95f887d85198d
Pablo González de la Parra <a01745096@itesm.mx>
2022-11-11 21:54:39 -0600

    Finishes user logic

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,18 +8,11 @@ router.get('/', userController.getAllUsers)
 router.post('/signUp', authController.signup);
 router.post('/signIn', authController.signin);
 
+// Generic routes (Admin)
 router
   .route('/:id')
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
-
-// Protect all routes after this middleware
-router.use(authController.protect);
-
-router.patch('/updateMyPassword', authController.updatePassword);
-router.get('/me', userController.getMe, userController.getUser);
-//router.patch('/updateMe', userController.updateMe);
-//router.delete('/deleteMe', userController.deleteMe);
 
 module.exports = router;
