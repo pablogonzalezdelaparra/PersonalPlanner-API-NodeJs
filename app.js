ea89244eaab318f895dbcaee0f9db7fd3e7d8ba7
Pablo González de la Parra <a01745096@itesm.mx>
2022-12-26 13:25:29 -0600

    Comments on app file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Imports
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -9,18 +10,19 @@ const userRouter = require('./routes/userRoutes');
 const eventRouter = require('./routes/eventRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 
+//Create an express app
 const app = express();
 
-// 1) GLOBAL MIDDLEWARES
-// Set security HTTP headers
+//Global middlewares
+//Set security HTTP headers
 app.use(helmet());
 
-// Development logging
+//Development status
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-// Limit requests from same API
+//Limit requests from same IP
 const limiter = rateLimit({
   max: 1000,
   windowMs: 60 * 60 * 1000,
@@ -28,34 +30,36 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// Body parser, reading data from body into req.body
+//Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
-// Data sanitization against NoSQL query injection
+//Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
-// Data sanitization against XSS
+//Data sanitization against XSS
 app.use(xss());
 
-// Serving static files
+//Serving static files (path)
 app.use(express.static(`${__dirname}/public`));
 
-// Test middleware
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
   next();
 });
 
-// 3) ROUTES
+//Routes
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/events', eventRouter);
 
-
+//Default route
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!
+  Please try a valid route`, 404));
 });
 
+//Error handling
 app.use(globalErrorHandler);
 
+//Exports
 module.exports = app;
