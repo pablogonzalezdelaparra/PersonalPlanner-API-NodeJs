5aebe387d37bb6e077f7309bf3fe524e77c387b5
Pablo González de la Parra <a01745096@itesm.mx>
2022-12-26 13:21:08 -0600

    Commenting server file

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
+//Imports
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+//Exception handling
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
+//Paths
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+//Database preparation
 const DB = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD
 );
 
+//Connection to database
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -22,11 +27,13 @@ mongoose
   })
   .then(() => console.log('DB connection successful!'));
 
+//Running server...
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+//Rejection handling
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
