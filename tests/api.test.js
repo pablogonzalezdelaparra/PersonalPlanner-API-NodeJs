f60d84001049e57117427ac151c5f1c6e6db2a23
Pablo González de la Parra <a01745096@itesm.mx>
2022-12-27 15:44:57 -0600

    Corrected and defined unit testing

--- tests/api.test.js
+++ tests/api.test.js
@@ -14,6 +14,9 @@ const DB = process.env.DATABASE.replace(
     process.env.DATABASE_PASSWORD
   );
 
+//Variables
+let oneEventId, oneEventDay, eventId, signupId, signupEmail, signInToken = null;
+
 //Connect to database before each test
 beforeEach(async () => {
     await mongoose.connect(DB);
@@ -24,36 +27,44 @@ afterEach(async () => {
   await mongoose.connection.close();
 });
 
+afterAll(async () => {
+  //TO-DO
+});
+
 //GET ROUTES
-//Events
+//All events
 describe("GET /api/v1/events", () => {
   it("should return all events", async () => {
     const res = await request(app).get("/api/v1/events");
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('success')
+
+    //Variables (external)
+    oneEventId = res.body.data.data[0]._id
+    oneEventDay = res.body.data.data[0].dayOfWeek
   });
 });
 
 //Events by weekday
-describe("GET /api/v1/events?weekday=sunday", () => {
-  it("should return all sunday events", async () => {
-    const res = await request(app).get("/api/v1/events?weekday=sunday");
+describe(`GET /api/v1/events?weekday=`, () => {
+  it("should return all events with a specific day of the week", async () => {
+    const res = await request(app).get(`/api/v1/events?weekday=${oneEventDay}`);
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('success')
   });
 });
 
 //Events by ID
-describe("GET /api/v1/events?_id=63977e330ae59c9fff94932f", () => {
+describe(`GET /api/v1/events?_id=`, () => {
   it("should return an event with a specific id", async () => {
-    const res = await request(app).get("/api/v1/events?_id=63977e330ae59c9fff94932f");
+    const res = await request(app).get(`/api/v1/events?_id=${oneEventId}`);
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('success')
   });
 });
 
 //POST ROUTES
-//Events
+//Create event
 describe("POST /api/v1/events", () => {
   it("should create an event", async () => {
     const res = await request(app).post("/api/v1/events").send({
@@ -63,6 +74,9 @@ describe("POST /api/v1/events", () => {
     });
     expect(res.statusCode).toBe(201);
     expect(res.body.status).toBe('success')
+
+    //Variables (external)
+    eventId = res.body.data.data._id;
   });
 });
 
@@ -75,12 +89,16 @@ describe("POST /api/v1/users/signup", () => {
       birthDate: "2000-08-11T00:00:00.000Z",
       city: "Buenos Aires",
       country: "Argentina",
-      email: "test@gmail.com",
+      email: "messi6@gmail.com",
       password: "password123",
       confirmPassword: "password123",
     });
     expect(res.statusCode).toBe(201);
     expect(res.body.status).toBe('success')
+
+    //Variables (external)
+    signupEmail = res.body.data.user.email;
+    signupId = res.body.data.user._id;
   });
 });
 
@@ -88,10 +106,34 @@ describe("POST /api/v1/users/signup", () => {
 describe("POST /api/v1/users/signin", () => {
   it("should sign in a current user", async () => {
     const res = await request(app).post("/api/v1/users/signin").send({
-      email: "test@gmail.com",
+      email: `${signupEmail}`,
       password: "password123",
     });
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBe('success')
+
+    //Variables (external)
+    signInToken = res.body.token
+  });
+});
+
+//DELETE ROUTES
+//Delete event
+describe(`DELETE /api/v1/events?id=`, () => {
+  it("should delete an event", async () => {
+    const res = await request(app)
+    .delete(`/api/v1/events?id=${eventId}`)
+    .set('authorization', `Bearer ${signInToken}`);
+    expect(res.statusCode).toBe(204);
+  });
+});
+
+//Delete user
+describe(`DELETE /api/v1/users?id=`, () => {
+  it("should delete a user", async () => {
+    const res = await request(app)
+    .delete(`/api/v1/users?id=${signupId}`)
+    .set('authorization', `Bearer ${signInToken}`);
+    expect(res.statusCode).toBe(204);
   });
 });
